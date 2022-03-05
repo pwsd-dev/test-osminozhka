89ff08bf612fb7acd6709d56d07ce26ec301e736
Nikita Shvetsov <nshvtsv@gmail.com>
2022-03-05 05:00:12 -0800

    add contacts

--- smartgrid.js
+++ smartgrid.js
@@ -1,49 +1,56 @@
 module.exports = {
-	columns: 12,
-	offset: "30px",
-	//mobileFirst: true,
-	container: {
-        maxWidth: "1630px",
-        fields: "15px"
+  columns: 12,
+  offset: "30px",
+  //mobileFirst: true,
+  container: {
+    maxWidth: "1822px",
+    fields: "15px",
+  },
+  breakPoints: {
+    laptopVeryBig: {
+      width: "1825px",
+      fields: "15px",
     },
-	breakPoints: {
-        laptopMid: {
-            width: "1550px",
-            fields: "15px"
-        },
-        laptopMd: {
-            width: "1367px",
-            fields: "15px"
-        },
-        laptopMini: {
-            width: "1200px",
-            fields: "15px"
-        },
-        ipad: {
-            width: "1024px",
-            fields: "15px"
-        },
-        md: {
-            width: "920px",
-            fields: "15px"
-        },
-		md: {
-            width: "920px",
-            fields: "15px"
-        },
-        sm: {
-            width: "720px"
-        },
-        xs: {
-            width: "576px"
-        },
-        xxs: {
-            width: "420px",
-            /*
-            offset: "10px",
-            fields: "5px"
-            */
-        }
-	},
-    //detailedCalc: true
+    laptopBig: {
+      width: "1680px",
+      fields: "15px",
+    },
+    laptopMid: {
+      width: "1580px",
+      fields: "15px",
+    },
+    laptopM: {
+      width: "1445px",
+      fields: "15px",
+    },
+    laptopMd: {
+      width: "1390px",
+      fields: "15px",
+    },
+    laptopMini: {
+      width: "1200px",
+      fields: "15px",
+    },
+    ipad: {
+      width: "1024px",
+      fields: "15px",
+    },
+    md: {
+      width: "920px",
+      fields: "15px",
+    },
+    sm: {
+      width: "768px",
+    },
+    xs: {
+      width: "576px",
+    },
+    xxs: {
+      width: "420px",
+
+      // offset: "10px",
+      // fields: "5px",
+    },
+  },
+  //detailedCalc: true
 };
